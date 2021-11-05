086082fbbc2af946c4f06da25537dee59bbde361
Luca Modica <luca.modica@edu.unito.it>
2021-11-05 12:50:11 +0100

    Added a draft UI

---
+++ demo/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.demo to javafx.fxml;
+    exports com.example.demo;
+}
