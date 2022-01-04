84b52e99991a1ce8859a95213b9483efddffff85
Oscar Naretto <oscar.naretto@edu.unito.it>
2022-01-04 17:26:08 +0100

    Adjusted server view draft

--- servermail/src/main/java/com/projprogiii/servermail/ServerApplication.java
+++ servermail/src/main/java/com/projprogiii/servermail/ServerApplication.java
@@ -11,8 +11,8 @@ public class ServerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ServerApplication.class.getResource("server.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Server!");
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        stage.setTitle("Mail Server");
         stage.setScene(scene);
         stage.show();
     }
