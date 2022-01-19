c3ba2d5317c1e8f7e4a58d2b5cf6f78112622571
Oscar Naretto <oscar.naretto@edu.unito.it>
2022-01-19 15:24:14 +0100

    Commands and relative database functions done.

--- servermail/src/main/java/com/projprogiii/servermail/server/session/command/DeleteEmail.java
+++ servermail/src/main/java/com/projprogiii/servermail/server/session/command/DeleteEmail.java
@@ -1,17 +1,19 @@
 package com.projprogiii.servermail.server.session.command;
 
+import com.projprogiii.lib.enums.ServerResponseName;
 import com.projprogiii.lib.objects.ClientRequest;
+import com.projprogiii.lib.objects.Email;
 import com.projprogiii.lib.objects.ServerResponse;
-
+import com.projprogiii.servermail.ServerApp;
 
 public class DeleteEmail extends Command {
 
     @Override
     public ServerResponse handle(ClientRequest req) {
-        //check email existence before
-
-
-
-        return null;
+        Email email = (Email) req.args().get(0);
+        if (email == null || !ServerApp.model.getDbManager().deleteEmail(email, req.auth())){
+            return  new ServerResponse(ServerResponseName.ILLEGAL_PARAMS, null);
+        }
+        return new ServerResponse(ServerResponseName.SUCCESS, null);
     }
 }
